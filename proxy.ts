b020615b66978de7dee8f5ca8771270e60ba8d82
xiaonan <xiaonan@example.com>
2026-03-02 09:30:28 +0800

    17 Homepage Components & Wrap Up

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
-// export { auth as proxy } from '@/auth';
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 
-export const { auth: proxy } = NextAuth(authConfig);
+const { auth: proxy } = NextAuth(authConfig);
+export default proxy;
